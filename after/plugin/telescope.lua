eb425ccc7f8698e04446f25d7553f18d0bb1d7ac
jokingmoony <penubokim@gmail.com>
2023-12-17 22:15:46 +0900

    First commit

---
+++ after/plugin/telescope.lua
@@ -0,0 +1,12 @@
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+-- normal
+-- insert
+-- visual
+
+-- vim.keymap.set({mode}, {lhs}, {rhs}, {opt})
+
---
+++ init.lua
@@ -0,0 +1,3 @@
+require('plugins')
+require('mapping')
+require('config')
