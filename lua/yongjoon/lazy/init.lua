d7f1bfe6028a473a3a9de71caebcefaca0e330f0
yongjoon <penubokim@gmail.com>
2026-02-26 23:38:08 +0900

    Remove mason and lsp keymap use native lsp

--- lua/yongjoon/lazy/init.lua
+++ lua/yongjoon/lazy/init.lua
@@ -23,3 +23,64 @@ require("lazy").setup("yongjoon.lazy.plugins", {
     notify = false,
   },
 })
+
+
+vim.lsp.enable('rust_analyzer')
+vim.lsp.enable('lua_ls')
+
+-- c lsp
+vim.lsp.config("clangd", {
+    cmd = {
+        "clangd",
+        "--background-index",
+    },
+    root_markers = { ".clangd", "compile_commands.json", "compile_flags.txt", ".git" },
+})
+
+vim.lsp.enable("clangd")
+
+-- python lsp
+vim.lsp.config("basepyright", {
+    cmd = { "basedpyright-langserver", "--stdio" },
+    filetypes = {"python"},
+    root_markers = {
+        "pyproject.toml",
+        "setup.py",
+        "setup.cfg",
+        "requirements.txt",
+        "Pipfile",
+        ".git"
+    },
+    settings = {
+        basedpyright = {
+            analysis = {
+                autoSearchPaths = true,
+                diagnosticMode = "openFilesOnly",
+                useLibraryCodeForTypes = true,
+                typeCheckingMode = "basic",
+            },
+        },
+    },
+})
+
+vim.lsp.enable("basedpyright")
+
+vim.lsp.config("ruff", {
+    cmd = {"ruff", "server"},
+    root_markers = {
+        "pyproject.toml",
+        "ruff.toml"
+    },
+})
+
+vim.lsp.enable("ruff")
+
+-- vim diagnostic hover setup
+vim.api.nvim_create_autocmd("CursorHold", {
+    buffer = bufnr,
+    callback = function()
+        vim.diagnostic.open_float(nil, { focusable = false })
+    end,
+})
+
+vim.opt.updatetime = 500
